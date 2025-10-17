102a7841f4e480d53ee7f9570e4793028225f4c7
weriomat <engel@weriomat.com>
2025-10-17 20:24:20 +0200

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      utils,
+      nixpkgs,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+          ];
+        };
+      }
+    );
+}
