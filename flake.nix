d189f02765394394eeb913fb401cd35490c6a95e
weriomat <engel@weriomat.com>
2025-10-17 20:28:59 +0200

    chore: add nix package

--- flake.nix
+++ flake.nix
@@ -17,6 +17,17 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        packages = utils.lib.flattenTree rec {
+          default = usbguard-dbus;
+          usbguard-dbus =
+            with pkgs;
+            buildGoModule {
+              name = "usbguard-dbus";
+              src = self;
+
+              vendorHash = "sha256-7s1mUMU7i+oEB7x5rq9T2G5ws3WmM1NQb/6SWb+Cg3Y=";
+            };
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.go
