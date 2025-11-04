63444c46274bc04dc6aa0afa4eb092aa53cfc5aa
weriomat <engel@weriomat.com>
2025-11-04 11:52:33 +0100

    chore: cleanup

--- server.go
+++ server.go
@@ -95,23 +95,6 @@ func (c *client) writePump() {
 				return
 			}
 			c.conn.WriteMessage(websocket.TextMessage, message)
-
-			// w, err := c.conn.NextWriter(websocket.TextMessage)
-			// if err != nil {
-			// 	return
-			// }
-			// w.Write(message)
-
-			// // Add queued chat messages to the current websocket message.
-			// n := len(c.send)
-			// for i := 0; i < n; i++ {
-			// 	w.Write(newline)
-			// 	w.Write(<-c.send)
-			// }
-
-			// if err := w.Close(); err != nil {
-			// 	return
-			// }
 		case <-ticker.C:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
